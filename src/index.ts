e92f3b2580b9e23006bc28e2c6f05c0a25c7deac
Sants16 <jvsantosrocha980@gmail.com>
2023-01-12 17:43:14 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello orld')
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, resp) => {
+  resp.send("hello");
+});
+
+app.listen(port, () => console.log(`port: ${port}`));
